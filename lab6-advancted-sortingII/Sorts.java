728700691b89c1f3f062183ef73c8e759e8b7d9a
Zahara-v <zahara.vazir99@gmail.com>
2020-05-04 18:04:14 +0100

    Lab 5 & 6 update

--- lab6-advancted-sortingII/Sorts.java
+++ lab6-advancted-sortingII/Sorts.java
@@ -17,9 +17,9 @@ public static void main(String args[]) {
 		
 		Random r = new Random();
 	
-		int[] arr = new int[100000];
-		for(int i=0; i<100000; i++) {
-			arr[i] = r.nextInt(100000);
+		int[] arr = new int[10];
+		for(int i=0; i<10; i++) {
+			arr[i] = r.nextInt(10);
 		}
 		
   	  int[] a = {0, 2, 2, 4, 0, 4, 5, 9, 1};  
@@ -27,15 +27,16 @@ public static void main(String args[]) {
 
 		  
 	     int n = arr.length;
-	     s.quickSort(arr, 0, n-1);
+//	     s.quickSort(arr, 0, n-1);
 	     
 //	     s.selectionSort(arr);
 //		  s.insertionSort(arr);
-//		  s.mergeSort(arr, arr.length);
+		  s.mergeSort(arr);
 //		  s.shuffle(arr); 
-	     
-	      
-		  
+//	     s.mergeSortEnhanced(arr);
+//	      s.quickSort(arr, 0, n-1);
+//	      s.enhancedQuickSort(arr, 0, n-1); 
+//		  
 	         
 		  System.out.print("Sorted array: "); 
 		  s.printArray(arr); 
@@ -62,61 +63,112 @@ public static void main(String args[]) {
 
 		//insert your MergeSort implementation here
 	    
-	    public static void mergeSort(int[] a, int n) {
-	    	
-	    	if(n<2) {
-	    		return;
-	    	}
-	    	
-	    	int mid = n/2;
-	    	int[] l = new int[mid]; 
-	    	int[] r = new int[n-mid]; 
-	    	
-	    	for(int i=0; i<mid; i++) {
-	    		l[i] = a[i];
-	    	}
-	    	for(int i=mid; i<n;i++) {
-	    		r[i-mid] = a[i];
-	    	}
-	    	mergeSort(l,mid); 
-	    	mergeSort(r, n-mid); 
-	    	
-	    	merge(a, l, r, mid, n-mid); 
+	    
+	    public static void mergeSort(int[] arr) {
+
+	        int n = arr.length;
+	        int middle = n / 2;
+	        int lSize = middle;
+	        int rSize = n - middle;
+	        int[] right = new int[rSize];
+	        int[] left = new int[lSize];
+	        
+	        if(n < 2) {
+	            return;
+	        }
+
+	            for(int i =0; i<middle; i++){
+	                left[i] = arr[i];
+	            }
+
+	            //creating subarrays
+	            for (int i = middle; i < n; i++) {
+	                right[i - middle] = arr[i];
+	            }
+
+	            mergeSort(left);
+	            mergeSort(right);
+	            merge(left, right, arr);
+	    } 
+	    
+	    public static void merge(int[] left, int[] right, int[] arr) {
+	        int rSize = right.length;
+	        int lSize = left.length;
+	        int i = 0, j = 0, k = 0;
+	        while (i < lSize && j < rSize) {
+	            if (left[i] <= right[j]) {
+	                arr[k] = left[i];
+	                i++;
+	                k++;
+	            } else {
+	                arr[k] = right[j];
+	                k++;
+	                j++;
+	            }
+	        }
+
+	        while (i < lSize) {
+	            arr[k] = left[i];
+	            k++;
+	            i++;
+	        }
+	        while (j < lSize) {
+	            arr[k] = right[j];
+	            k++;
+	            j++;
+	        }
 	    }
 	    
-	    public static void merge(int[]a, int[]l, int[]r, int left, int right) {
-	    	
-	    	int i=0, j=0, k=0;
-	    	
-	    	while(i<left && j<right) {
-	    		if(l[i] <= r[j]) {
-	    			a[k++] = l[i++];
-	    		}
-	    		else {
-	    			a[k++] = r[j++];
-	    		}
-	    	}
-	    	
-	    	while(i<left) {
-	    		a[k++] = l[i++];
-	    	}
-	    	
-	    	while(j<right) {
-	    		a[k++] = r[j++]; 
-	    	}
-	    }
-	   
-	    public void quickSort(int arr[], int low, int high) {
-			if(low<high) {
-				int p = partition(arr, low, high);
-				
-				quickSort(arr, low, p-1);
-				quickSort(arr, p+1, high);
-			}
-		}
-	  
+	    
+	    public static void mergeSortEnhanced(int[] arr){
+	        int n = arr.length;
+	        int mid = n/2;
+	        if (n <= 8){    
+	            insertionSort(arr);
+	            return;
+	        }
 
-	    public int partition(int arr[], int low, int high) {
+	        int[] left = new int[mid];
+	        int[] right = new int[n-mid];
+
+	        for (int i=0;i<mid;i++){
+	            left[i] = arr[i];
+	        }
+
+	        for (int i=mid;i<n;i++){
+	            right[i-mid] = arr[i];
+	        }
+	        mergeSort(left);
+	        mergeSort(right);
+
+	        if (left[mid-1] <= right[0]){
+	            for(int pos=0;pos<mid;pos++){
+	                arr[pos] = left[pos];
+	            }
+
+	            for (int pos=mid;pos<n;pos++){
+	                arr[pos] = right[pos-mid];
+	            }
+	        }else {
+	            merge(left, right, arr);
+	        }
+	}
+	  
+	    public static void quickSort(int arr[], int low, int high) {
+
+	        for (int i = low + 1; i <= high; i++) {
+	            int key = arr[i];
+	            int j = i;
+
+	            while (j > low && arr[j-1] > key) {
+	                arr[j] = arr[j - 1];
+	                j--;
+	            }
+	            arr[j] = key;
+	        }
+	}
+
+	    public static int partition(int arr[], int low, int high) {
 			int pivot = arr[high];
 			int i = (low-1);
 			int temp;
@@ -134,6 +186,43 @@ public static void main(String args[]) {
 			arr[high] = temp;
 			return (i+1);
 		}
+	    
+	    
+	    public static void enhancedQuickSort(int[] arr, int low, int high){
+
+	        if (high <= low+10){
+	            //System.out.println("insertion used ");
+	            quickSort(arr,low,high);
+	            return;
+	        }else {
+	            int mid = (low + high)/2;
+	            if (arr[mid] < arr[low]){partition(arr, low, mid);}
+	            if (arr[high] < arr[low]){partition(arr, low, high);}
+	            if (arr[high] < arr[mid]){partition(arr, mid, high);}
+
+	            //now put the pivot at last position
+	            partition(arr, mid, high-1);
+	            int pivot = arr[high-1];
+
+	            // Begin partitioning
+	            int i, j;
+	            for( i = low, j = high - 1; ; ) {
+	                while( arr[++i]<pivot)
+	                    ;
+	                while( pivot < arr[--j])
+	                    ;
+	                if( i >= j )
+	                    break;
+	                partition( arr, i, j );
+	            }
+
+	            // Restore pivot
+	            partition( arr, i, high - 1 );
+
+	            enhancedQuickSort( arr, low, i - 1 );    // Sort small elements
+	            enhancedQuickSort( arr, i + 1, high );   // Sort large elements
+	        }
+	}
 	    
 		// ***************************** Insertion Sorts *****************************
 
